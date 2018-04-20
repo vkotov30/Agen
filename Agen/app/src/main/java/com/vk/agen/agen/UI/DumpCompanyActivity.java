1f6c7a5857d9f35a44d6f2e256d0b777e4f94ade
Vladislav Kotov <vkotov30@gmail.com>
2018-04-20 17:49:21 +0300

    begining

---
+++ Agen/app/src/main/java/com/vk/agen/agen/UI/DumpCompanyActivity.java
@@ -0,0 +1,15 @@
+package com.vk.agen.agen.UI;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.vk.agen.agen.R;
+
+public class DumpCompanyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate( savedInstanceState );
+        setContentView( R.layout.activity_dump_company );
+    }
+}
---
+++ Agen/app/src/main/java/com/vk/agen/agen/UI/ReportActivity.java
@@ -0,0 +1,15 @@
+package com.vk.agen.agen.UI;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.vk.agen.agen.R;
+
+public class ReportActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate( savedInstanceState );
+        setContentView( R.layout.activity_report );
+    }
+}
